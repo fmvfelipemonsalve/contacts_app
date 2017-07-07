e60731075aeb5a1ea0222045fbdb30f38449155f
Felipe Monsalve <fmonsalve@fb.com>
2017-07-06 17:47:57 -0700

    first commit

---
+++ app/src/main/java/com/codepath/android/lollipopexercise/activities/DetailsActivity.java
@@ -0,0 +1,101 @@
+package com.codepath.android.lollipopexercise.activities;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.graphics.Palette;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.animation.GlideAnimation;
+import com.bumptech.glide.request.target.SimpleTarget;
+import com.codepath.android.lollipopexercise.R;
+import com.codepath.android.lollipopexercise.models.Contact;
+
+public class DetailsActivity extends AppCompatActivity {
+    public static final String EXTRA_CONTACT = "EXTRA_CONTACT";
+    private Contact mContact;
+    private ImageView ivProfile;
+    private TextView tvName;
+    private TextView tvPhone;
+    private View vPalette;
+
+    private FloatingActionButton fab;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details);
+
+        ivProfile = (ImageView) findViewById(R.id.ivProfile);
+        tvName = (TextView) findViewById(R.id.tvName);
+        tvPhone = (TextView) findViewById(R.id.tvPhone);
+        vPalette = findViewById(R.id.vPalette);
+
+        // Extract contact from bundle
+        mContact = (Contact)getIntent().getSerializableExtra(EXTRA_CONTACT);
+
+        // Fill views with data
+        Glide.with(DetailsActivity.this).load(mContact.getThumbnailDrawable()).centerCrop().into(ivProfile);
+        tvName.setText(mContact.getName());
+        tvPhone.setText(mContact.getNumber());
+
+        SimpleTarget<Bitmap> target = new SimpleTarget<Bitmap>() {
+            @Override
+            public void onResourceReady(Bitmap resource, GlideAnimation<? super Bitmap> glideAnimation) {
+                // Insert the bitmap into the profile image view
+                ivProfile.setImageBitmap(resource);
+                // Use generate() method from the Palette API to get the vibrant color from the bitmap
+                Palette.from(resource).generate(new Palette.PaletteAsyncListener() {
+                    @Override
+                    public void onGenerated(Palette palette) {
+                        // Set the result as the background color for `R.id.vPalette` view containing the contact's name.
+                        Palette.Swatch swatch = palette.getVibrantSwatch();
+                        if (swatch!=null) {
+                            int color = swatch.getRgb();
+                            vPalette.setBackgroundColor(color);
+                        }
+                    }
+                });
+            }
+        };
+        // Clear the background color in adapter
+        vPalette.setBackgroundColor(0);
+
+        // Store the target into the tag for the profile to ensure target isn't garbage collected prematurely
+        ivProfile.setTag(target);
+        // Instruct Picasso to load the bitmap into the target defined above
+        Glide.with(this).load(mContact.getThumbnailDrawable()).asBitmap().centerCrop().into(target);
+
+        // save reference to call button
+        fab = (FloatingActionButton) findViewById(R.id.fab);
+
+        //set action listener for calling a phone number
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String uri = "tel:" + mContact.getNumber();
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse(uri));
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finishAfterTransition();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+}
---
+++ app/src/main/java/com/codepath/android/lollipopexercise/adapters/ContactsAdapter.java
@@ -0,0 +1,127 @@
+package com.codepath.android.lollipopexercise.adapters;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.support.v4.app.ActivityOptionsCompat;
+import android.support.v4.util.Pair;
+import android.support.v7.graphics.Palette;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.animation.GlideAnimation;
+import com.bumptech.glide.request.target.SimpleTarget;
+import com.codepath.android.lollipopexercise.R;
+import com.codepath.android.lollipopexercise.activities.DetailsActivity;
+import com.codepath.android.lollipopexercise.models.Contact;
+
+import java.util.List;
+
+
+// Provide the underlying view for an individual list item.
+public class ContactsAdapter extends RecyclerView.Adapter<ContactsAdapter.VH> {
+    private Activity mContext;
+    private List<Contact> mContacts;
+
+    public ContactsAdapter(Activity context, List<Contact> contacts) {
+        mContext = context;
+        if (contacts == null) {
+            throw new IllegalArgumentException("contacts must not be null");
+        }
+        mContacts = contacts;
+    }
+
+    // Inflate the view based on the viewType provided.
+    @Override
+    public VH onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_contact, parent, false);
+        return new VH(itemView, mContext);
+    }
+
+    // Display data at the specified position
+    @Override
+    public void onBindViewHolder(final VH holder, int position) {
+        Contact contact = mContacts.get(position);
+        holder.rootView.setTag(contact);
+        holder.tvName.setText(contact.getName());
+
+        SimpleTarget<Bitmap> target = new SimpleTarget<Bitmap>() {
+            @Override
+            public void onResourceReady(Bitmap resource, GlideAnimation<? super Bitmap> glideAnimation) {
+                // Insert the bitmap into the profile image view
+                holder.ivProfile.setImageBitmap(resource);
+                // Use generate() method from the Palette API to get the vibrant color from the bitmap
+                Palette.from(resource).generate(new Palette.PaletteAsyncListener() {
+                    @Override
+                    public void onGenerated(Palette palette) {
+                        // Set the result as the background color for `R.id.vPalette` view containing the contact's name.
+                        Palette.Swatch swatch = palette.getVibrantSwatch();
+                        if (swatch!=null) {
+                            int color = swatch.getRgb();
+                            holder.vPalette.setBackgroundColor(color);
+                        }
+                    }
+                });
+            }
+        };
+        // Clear the background color in adapter
+        holder.vPalette.setBackgroundColor(0);
+
+        // Store the target into the tag for the profile to ensure target isn't garbage collected prematurely
+        holder.ivProfile.setTag(target);
+        // Instruct Picasso to load the bitmap into the target defined above
+        Glide.with(mContext).load(contact.getThumbnailDrawable()).asBitmap().centerCrop().into(target);
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mContacts.size();
+    }
+
+    // Provide a reference to the views for each contact item
+    public class VH extends RecyclerView.ViewHolder {
+        final View rootView;
+        final ImageView ivProfile;
+        final TextView tvName;
+        final View vPalette;
+
+        public VH(View itemView, final Context context) {
+            super(itemView);
+            rootView = itemView;
+            ivProfile = (ImageView)itemView.findViewById(R.id.ivProfile);
+            tvName = (TextView)itemView.findViewById(R.id.tvName);
+            vPalette = itemView.findViewById(R.id.vPalette);
+
+            // Navigate to contact details activity on click of card view.
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    final Contact contact = (Contact)v.getTag();
+                    if (contact != null) {
+                        // Fire an intent when a contact is selected
+                        Intent i = new Intent(context, DetailsActivity.class);
+                        // Pass contact object in the bundle and populate details activity.
+                        i.putExtra("EXTRA_CONTACT",contact);
+
+                        Intent intent = new Intent(context, DetailsActivity.class);
+                        intent.putExtra(DetailsActivity.EXTRA_CONTACT, contact);
+                        Pair<View, String> p1 = Pair.create((View)ivProfile, "profile");
+                        Pair<View, String> p2 = Pair.create(vPalette, "palette");
+                        Pair<View, String> p3 = Pair.create((View)tvName, "text");
+                        ActivityOptionsCompat options = ActivityOptionsCompat.makeSceneTransitionAnimation(mContext,p1,p2,p3);
+                        context.startActivity(intent, options.toBundle());
+
+                    }
+                }
+            });
+        }
+    }
+}
